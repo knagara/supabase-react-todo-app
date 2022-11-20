ce39ef92bf41c7109e3fbdaddc7a2ee984c4eeb1
knagara <nagara.keita@gmail.com>
2022-11-20 21:48:19 +0900

    タスクの削除

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -49,7 +49,7 @@ export default function TaskList() {
             <Text w="100%" p="8px" borderRadius="lg">
               {task.text}
             </Text>
-            <DeleteTask />
+            <DeleteTask id={task.id} />
           </HStack>
         ))}
       </VStack>
