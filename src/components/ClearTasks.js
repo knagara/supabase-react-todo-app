dbc49fc9877afcd4f57665a816e719a72050651a
knagara <nagara.keita@gmail.com>
2022-11-20 22:22:28 +0900

    タスクの全削除

--- src/components/ClearTasks.js
+++ src/components/ClearTasks.js
@@ -1,10 +1,30 @@
 import { Flex, Button, useToast } from '@chakra-ui/react';
 import { useState } from 'react';
+import supabase from '../supabase';
 
 export default function ClearTasks() {
+  const [loading, setLoading] = useState(false);
+  async function handleClear() {
+    setLoading(true);
+    const { data, error } = await supabase
+      .from('todos')
+      .delete()
+      .not('text', 'eq', 'Do not delete me');
+    setLoading(false);
+  }
+
   return (
     <Flex>
-      <Button colorScheme="gray" px="8" h="45" color="gray.500" mt="10">
+      <Button
+        colorScheme="gray"
+        px="8"
+        h="45"
+        color="gray.500"
+        mt="10"
+        onClick={handleClear}
+        isLoading={loading}
+        loadingText="Clearing..."
+      >
         Clear Tasks
       </Button>
     </Flex>
